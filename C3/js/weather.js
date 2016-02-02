6cae36de4f55dfbd04b019aa081fa0cf60a92563
pripper <stromvall__@hotmail.com>
2016-02-02 18:08:06 +0100

    C3 work

---
+++ C3/js/weather.js
@@ -0,0 +1,40 @@
+if("geolocation" in navigator){
+	navigator.geolocation.getCurrentPosition(function(position) {
+		loadWeather(position.coords.latitude + ',' + position.cords.longitude); 
+	});
+} else {
+	loadWeather("Kolkata, IN", "");
+	
+	
+}
+	
+$(document).ready(function() {
+	setInterval(getWeather, 10000);
+	
+});
+
+function loadweather(location, woeid) {
+	$.simpleWeather ({
+		location: location,
+		woeid: woeid,
+		unit: 'c',
+		succes: function(weather) {
+			city = weather.city;
+			temp = weather.temp+'&deg';
+			wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">' ;
+			wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
+			humadity = weather.humadity + ' %';
+			
+$(".location").text(city);
+$(".temperature").html(temp);
+$(".climate_bg").html(wcode);
+$(".windspeed").html(wind);
+$(".humadity").text(humadity);
+		},
+		error: function(error) {
+			$(".error").html('<p>' + error + '</p>');
+		}
+	});
+			
+	
+}
