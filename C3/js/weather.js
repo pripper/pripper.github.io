2320f8aa79cefedf99e242d31fffa5dc1810f8a6
pripper <stromvall__@hotmail.com>
2016-02-05 18:38:51 +0100

    Revert "update"
    This reverts commit 06efe686ae71729c3147a46bb30a5e2dadc372ae.

--- C3/js/weather.js
+++ C3/js/weather.js
@@ -26,13 +26,13 @@
 			temp = weather.temp+'&deg';
 			wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">' ;
 			wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
-			//humadity = weather.humadity + ' %';
+			humadity = weather.humadity + ' %';
 			
 $(".location").text(city);
 $(".temperature").html(temp);
 $(".climate_bg").html(wcode);
 $(".windspeed").html(wind);
-//$(".humadity").text(humadity);
+$(".humadity").text(humadity);
     },
     error: function(error) {
       $(".weather").html('<p>'+error+'</p>');
